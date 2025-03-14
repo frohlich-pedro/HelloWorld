fec62a2d44fcbf8f42b399a910902f7e39e47e78
frohlich-pedro <pedro_h_nf@outlook.com>
2025-03-14 18:47:25 +0000

    Update - HelloWorld.js I rewrote the server using the OOP paradigm

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,35 +1,56 @@
 require('dotenv').config();
-const http = require('http');
-const serveStatic = require('serve-static');
+const http         = require('http');
+const serveStatic  = require('serve-static');
 const finalhandler = require('finalhandler');
-const path = require('path');
-const morgan = require('morgan');
+const path         = require('path');
+const morgan       = require('morgan');
 
-const hostname = process.env.HOSTNAME;
-const port = process.env.PORT;
-const serve = serveStatic(path.join(__dirname, 'www'));
-const logger = morgan('combined');
+class Server {
+    constructor() {
+        this.hostname = process.env.HOSTNAME;
+        this.port     = process.env.PORT;
+        this.serve    = serveStatic(path.join(__dirname, 'www'));
+        this.logger   = morgan('combined');
+        this.server   = http.createServer(this.requestHandler.bind(this));
+        this.validateEnvironmentVariables();
+    }
 
-const server = http.createServer((req, res) => {
-    logger(req, res, (err) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-            return;
+    validateEnvironmentVariables() {
+        if (!this.hostname || !this.port) {
+            console.error('HOSTNAME e PORT devem ser definidos no arquivo .env');
+            process.exit(1);
         }
+    }
 
-        serve(req, res, (err) => {
+    requestHandler(req, res) {
+        this.logger(req, res, (err) => {
             if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('Not Found');
+                this.sendError(res, 500, 'Internal Server Error');
                 return;
             }
-            
-            finalhandler(req, res)(err);
-        });
-    });
-});
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+            this.serve(req, res, (err) => {
+                if (err) {
+                    this.sendError(res, 404, 'Not Found');
+                    return;
+                }
+                
+                finalhandler(req, res)(err);
+            });
+        });
+    }
+
+    sendError(res, statusCode, message) {
+        res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+        res.end(message);
+    }
+
+    start() {
+        this.server.listen(this.port, this.hostname, () => {
+            console.log(`Server running at http://${this.hostname}:${this.port}/`);
+        });
+    }
+}
+
+const serverInstance = new Server();
+serverInstance.start();
