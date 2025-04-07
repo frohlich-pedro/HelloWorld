dcb89c8126933da6e2a9d813612f6e093fcaeb02
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-04-07 20:53:08 -0300

    Update and rename HelloWorld.c to HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,41 @@
+class HelloWorld {
+public:
+  explicit HelloWorld() {
+    kernelInit();
+  }
+
+  ~HelloWorld() = default;
+
+private:
+  void kernelInit() {
+    char* video = (char*)0xb8000;
+    unsigned int i = 0;
+    unsigned int j = 0;
+
+    while (i < (80 * 25 * 2)) {
+      *(video + i) = ' ';
+      *(video + (i + 1)) = 0x07;
+      i += 2;
+    }
+
+    const char* string = "Oiee :3";
+    i = (j * 80 * 2);
+
+    while (*(string) != 0) {
+      if (*string == '\n') {
+        j += 1;
+        i = (j * 80 * 2);
+        string += 1;
+      } else {
+        *(video + i) = *(string);
+        *(video + (i + 1)) = 0x07;
+        string += 1;
+        i += 2;
+      }
+    }
+  }
+};
+
+extern "C" void kernelMain() {
+  HelloWorld helloWorld;
+}
