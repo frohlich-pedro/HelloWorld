e82fd3d93fee82549adc952f391882ed07491ddf
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-04-07 20:56:41 -0300

    Update HelloWorld.asm

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -1,14 +1,14 @@
 bits 32
 section .text
 	align 4
-	dd 0x1BADB002			;magic
-	dd 0x00				;flags
-	dd - (0x1BADB002 + 0x00)	;checksum. m+f+c should be zero
+	dd 0x1BADB002
+	dd 0x00
+	dd - (0x1BADB002 + 0x00)
 
 global start
-extern kernelMain	;k_main is defined in the kernel.c file
+extern kernelMain
 
 start:
-	cli  ; stop interrupts
+	cli
 	call kernelMain
-	hlt ; halt the CPU
+	hlt
