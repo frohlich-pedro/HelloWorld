f20acc11bc3f23f35e3defe78a824cd019b3d4f9
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-04-07 21:16:24 -0300

    Update sayItOutLoud.sh

--- sayItOutLoud.sh
+++ sayItOutLoud.sh
@@ -1,5 +1,5 @@
 set -e
 nasm -f elf32 HelloWorld.asm -o HelloWorld-asm.o
-g++ -m32 -c HelloWorld.cpp -o HelloWorld.o
+g++ -Os -m32 -c HelloWorld.cpp -o HelloWorld.o
 ld -m elf_i386 -T linker.ld -o HelloWorld.bin HelloWorld-asm.o HelloWorld.o
 qemu-system-i386 -kernel HelloWorld.bin
