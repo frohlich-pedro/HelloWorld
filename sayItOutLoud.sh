852e9cdb3ae81578926c5d9db58714fb1e05c379
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-04-11 16:18:23 -0300

    Update sayItOutLoud.sh

--- sayItOutLoud.sh
+++ sayItOutLoud.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 set -e
-
-# Create output directory if it doesn't exist
 mkdir -p out
-
-# Assemble the Assembly code
 nasm -f elf32 src/HelloWorld.asm -o out/HelloWorld-asm.o
-
-# Compile the C++ source code
 g++ -Os -m32 -c src/HelloWorld.cpp -o out/HelloWorld.o
-
-# Link the object files into a binary
 ld -m elf_i386 -T linker.ld -o out/HelloWorld.bin out/HelloWorld-asm.o out/HelloWorld.o
-
-# Run the binary in QEMU
 qemu-system-i386 -kernel out/HelloWorld.bin
